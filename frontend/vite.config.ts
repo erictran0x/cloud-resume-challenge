0d99c452509fa7ae3044bbe3052b61329273e2bb
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-11-02 14:22:21 -0800

    remove hashes in build files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../terraform/modules/frontend/s3_site',
+    rollupOptions: {
+      output: {
+        // Customize filename patterns for different types of files
+        entryFileNames: `assets/[name].js`, // For entry chunks (e.g., main.js)
+        chunkFileNames: `assets/[name].js`, // For dynamic import chunks
+        assetFileNames: `assets/[name].[ext]`, // For other assets like CSS, images, fonts
+      },
+    },
+  }
 })
