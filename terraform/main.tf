c5d20f1e451efe2dd9e7920574de5f8d74693072
erictran0x <40257345+erictran0x@users.noreply.github.com>
2025-11-06 14:35:48 -0800

    setup github actions for frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,15 @@ terraform {
 			version = "~> 6.0"
 		}
 	}
+	backend "s3" {
+		bucket 	= "terraform-states-erictran"
+		key 		= "erictran.link/terraform.tfstate"
+		region 	= "us-west-1"
+
+		assume_role = {
+			role_arn = "arn:aws:iam::058264485635:role/terraform"
+		}
+	}
 }
 
 provider "aws" {
